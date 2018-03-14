aecf2b2b1528626cb84243586ec794564468ce12
Maxim Avezbakiev <maxim@snowleopardcloud.co.uk>
2018-03-14 22:28:23 +0000

    remove sql_scripts from repo and move docker image creation to a script.

---
+++ mysql_migration/build-image.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#create sql_scripts directory
+mkdir -p sql_scripts
+#copy the SQL files to the current directory
+cp ../src/main/resources/db/mysql/schema.sql ./sql_scripts/schema.sql
+cp ../src/main/resources/db/mysql/data.sql ./sql_scripts/data.sql
+
+echo "Starting the build..."
+docker build -t mysql-db-migration:latest .
